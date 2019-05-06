c041afed6a5d81743a18cb1bde5277695f978270
kyrader <kyrader@microsoft.com>
2019-05-06 09:16:26 -0700

    Start Pride.dll and PrideCli

---
+++ Pride/PrideConsole.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Pride
+{
+    public class PrideConsole
+    {
+        public static readonly ConsoleColor[] Rainbow = new ConsoleColor[]
+        {
+            ConsoleColor.Red,
+            ConsoleColor.Yellow,
+            ConsoleColor.Green,
+            ConsoleColor.Blue,
+            ConsoleColor.DarkMagenta,
+        };
+
+        public static void WriteWords(string input, string finalNewline = null)
+        {
+            var words = input.Split();
+            int colorIndex = 0;
+            int wordCount = 0;
+            var oldBackground = Console.BackgroundColor;
+            var oldForeground = Console.ForegroundColor;
+            Console.BackgroundColor = ConsoleColor.White;
+            foreach (var word in words)
+            {
+                wordCount++;
+                Console.ForegroundColor = Rainbow[colorIndex];
+                Console.Write(word);
+                if (wordCount < words.Length)
+                {
+                    Console.Write(" ");
+                }
+                colorIndex = (colorIndex+1) % (Rainbow.Length-1);
+            }
+            if (!string.IsNullOrWhiteSpace(finalNewline))
+            {
+                Console.Write(finalNewline);
+            }
+            Console.BackgroundColor = oldBackground;
+            Console.ForegroundColor = oldForeground;
+        }
+    }
+}
---
+++ PrideCli/Program.cs
@@ -0,0 +1,13 @@
+﻿using Pride;
+
+namespace PrideCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse porta, tellus a accumsan elementum, purus justo imperdiet arcu, et faucibus quam urna vitae quam. Nullam quam nisi, condimentum in rutrum nec, tristique ac ligula. Donec eget malesuada sapien. Maecenas ornare egestas suscipit. In vel eros sed massa efficitur congue nec eget massa. Donec pretium ipsum tempus quam porttitor, nec placerat tortor blandit. Duis lacinia vitae nunc at molestie. Maecenas rhoncus justo nec scelerisque egestas. Phasellus venenatis tortor et urna placerat tincidunt. Integer commodo, odio non suscipit facilisis, enim eros varius arcu, quis feugiat dolor mi vitae elit. Mauris non egestas arcu. In accumsan dolor in mauris viverra tincidunt. Sed bibendum enim quis justo lacinia, quis maximus turpis tempus.";
+            PrideConsole.WriteWords(text);
+        }
+    }
+}
